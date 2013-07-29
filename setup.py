5868b01ddc5aa8877421745edb1426f22e8323f4
Mathieu Agopian <mathieu.agopian@gmail.com>
2013-07-29 18:21:42 +0200

    python3 compatibility

--- setup.py
+++ setup.py
@@ -25,5 +25,6 @@ setup(
         'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
     ],
 )
